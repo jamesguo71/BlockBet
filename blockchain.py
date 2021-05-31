0997740604efafca109899cdba107fbbc4e673b5
Fei <i.james.guo@gmail.com>
2021-05-31 17:19:11 +0800

    decrease difficulty level

--- blockchain.py
+++ blockchain.py
@@ -8,7 +8,7 @@ from typing import List
 
 from peer import Peer
 from message import MessageType, block_header_fmt, bet_fmt
-ZEROS = 18 # About 20 seconds of mining on average for this difficulty level
+ZEROS = 16 # About 20 seconds of mining on average for this difficulty level
 
 GENESIS_HASH = hashlib.sha256(bytes("0b" + 256 * '0', "ascii")).digest()
 
