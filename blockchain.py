ec4b70e629d7200fc902ace0fe90d85dbd6ecbe5
Fei <i.james.guo@gmail.com>
2021-05-31 13:43:12 +0800

    genesis hash update

--- blockchain.py
+++ blockchain.py
@@ -9,7 +9,7 @@ from peer import Peer
 from message import MessageType, block_header_fmt, bet_fmt
 ZEROS = 19 # About 20 seconds of mining on average for this difficulty level
 
-GENESIS_HASH = bytes("0b" + 256 * '0', "ascii")
+GENESIS_HASH = hashlib.sha256(bytes("0b" + 256 * '0', "ascii")).digest()
 
 class Block:
 
