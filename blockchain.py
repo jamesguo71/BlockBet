4e9b4e644cce0619ad693faace0cec240c449502
Fei <i.james.guo@gmail.com>
2021-05-31 18:43:09 +0800

    Static method explicitly declared; Enable peer signature check again;

--- blockchain.py
+++ blockchain.py
@@ -8,7 +8,9 @@ from typing import List
 
 from peer import Peer
 from message import MessageType, block_header_fmt, bet_fmt
-ZEROS = 16 # About 20 seconds of mining on average for this difficulty level
+
+# Hoping for 20secs per block with this difficulty level, but depends on host machines
+ZEROS = 16
 
 GENESIS_HASH = hashlib.sha256(bytes("0b" + 256 * '0', "ascii")).digest()
 
@@ -26,7 +28,8 @@ class Blockchain:
 
     def __init__(self, peer: Peer):
         self.peer = peer
-        self.blockchain = [] # type: List[Block]
+        self.blockchain = []  # type: List[Block]
+        self.mining_thread = None
         self.is_mining = False
         self.stop_mining = False
 
@@ -47,16 +50,16 @@ class Blockchain:
     def ibd_response_handler(self, data, src):
         temp_blockchain = []
         prev_hash = GENESIS_HASH
-        start = struct.calcsize("I") # message type
+        start = struct.calcsize("I")  # message type
         hdr_size = struct.calcsize(block_header_fmt)
         while start < len(data):
-            if not self.verify_header(prev_hash, data[start:start+hdr_size]):
+            if not self.verify_header(prev_hash, data[start:start + hdr_size]):
                 print("[IBD] ibd: Header verification failed")
                 break
             else:
-                nbytes, block = self._receive_block(prev_hash, data[start:])
-                print("[IBD] Received IBD block of", nbytes, "bytes")
-                start += nbytes
+                n_bytes, block = self._receive_block(data[start:])
+                print("[IBD] Received IBD block of", n_bytes, "bytes")
+                start += n_bytes
                 prev_hash = self.calc_prev_hash(block)
                 temp_blockchain.append(block)
         if len(self.blockchain) == 0 or len(temp_blockchain) > len(self.blockchain):
@@ -76,36 +79,37 @@ class Blockchain:
             self.is_mining = True
             self.mining_thread.start()
 
-
-    def _receive_block(self, prev_hash, data):
+    @staticmethod
+    def _receive_block(data):
         hdr_size = struct.calcsize(block_header_fmt)
-        nbytes = hdr_size
+        n_bytes = hdr_size
         prev_hash, timestamp, nonce, bet_num = struct.unpack_from(block_header_fmt, data)
         bets = []
         for _ in range(bet_num):
             bet, = struct.unpack_from(bet_fmt, data[hdr_size:])
             bets.append(bet)
-            nbytes += struct.calcsize(bet_fmt)
-        return nbytes, Block(prev_hash, timestamp, nonce, bet_num, bets)
+            n_bytes += struct.calcsize(bet_fmt)
+        return n_bytes, Block(prev_hash, timestamp, nonce, bet_num, bets)
 
     def push_my_blockchain(self, data, src):
+        assert struct.unpack_from("I", data) == MessageType.IBD_REQUEST
         response = struct.pack("I", MessageType.IBD_RESPONSE)
         for block in self.blockchain:
             response += self.add_block_bytes(block)
         print("[IBD] Sending whole blockchain to target", src)
         self.peer.send_signed_data(response, src)
 
-
-    def add_block_bytes(self, block):
+    @staticmethod
+    def add_block_bytes(block):
         ret = struct.pack(block_header_fmt, block.prev_hash, block.timestamp,
-                                block.nonce, block.bet_num)
+                          block.nonce, block.bet_num)
         for i in range(block.bet_num):
             ret += struct.pack(bet_fmt, block.bets[i])
         return ret
 
     def receive_new_block(self, data, src):
-        start = struct.calcsize("I") # message type
-        data = data[start:] # skip message type
+        start = struct.calcsize("I")  # message type
+        data = data[start:]  # skip message type
 
         hdr_size = struct.calcsize(block_header_fmt)
         if len(self.blockchain) > 0:
@@ -123,7 +127,7 @@ class Blockchain:
                 bet, = struct.unpack_from(bet_fmt, data[hdr_size:])
                 bets.append(bet)
             self.blockchain.append(Block(prev_hash, timestamp, nonce, bet_num, bets))
-            print("[SUCCESS] Received a new valid block from network")
+            print("[SUCCESS] Received a new valid block from ", src)
             self.restart_mining()
 
     def mining(self):
@@ -137,7 +141,8 @@ class Blockchain:
         nonce = 0
         while not self.stop_mining:
             nonce += 1
-            print("\r[INFO] Current block height: %s, " % len(self.blockchain) + "mining progress: %s" % nonce, end = "")
+            print("\r[INFO] Current block height: %s, " % len(self.blockchain)
+                  + "mining progress: %s" % nonce, end="")
             header = struct.pack(block_header_fmt, prev_hash, timestamp, nonce, bet_num)
             if self.verify_nonce(header):
                 print("[INFO] mining succeeded.")
@@ -149,8 +154,10 @@ class Blockchain:
                 prev_hash, nonce, timestamp, bet_num = \
                     self.calc_prev_hash(new_block), 0, int(time.time()), 4
 
-    def calc_prev_hash(self, block):
-        header = struct.pack(block_header_fmt, block.prev_hash, block.timestamp, block.nonce, block.bet_num)
+    @staticmethod
+    def calc_prev_hash(block):
+        header = struct.pack(block_header_fmt, block.prev_hash,
+                             block.timestamp, block.nonce, block.bet_num)
         dgst = hashlib.sha256(header).digest()
         return dgst
 
@@ -162,20 +169,16 @@ class Blockchain:
     def verify_header(self, prev_hash, block_header):
         logging.debug("prev_hash", prev_hash)
         logging.debug("block_header", block_header)
-        logging.debug("unpacked", struct.unpack_from("<32s", block_header) )
+        logging.debug("unpacked", struct.unpack_from("<32s", block_header))
         if struct.unpack_from("<32s", block_header)[0] != prev_hash:
             return False
         logging.debug("hash ok, then nonce")
         return self.verify_nonce(block_header)
 
-    def verify_nonce(self, block_header):
+    @staticmethod
+    def verify_nonce(block_header):
         dgst = hashlib.sha256(block_header).digest()
         bin_str = '{:0256b}'.format(int(binascii.hexlify(dgst), 16))
         if bin_str[:ZEROS] == '0' * ZEROS:
             return True
         return False
-
-    def verify_chain(self, chain):
-        pass
-
-
