9ab0d99a3be455ba5516fb20f8c97ce6b561866b
Angus Emmett <gusemmett@gmail.com>
2021-06-02 15:48:04 -0400

    Added look to make bet

--- gui.py
+++ gui.py
@@ -1,20 +1,32 @@
 import PySimpleGUI as sg
 from testAPI import *
 
-
+td = {"auto_size_text" : False, "justification":"c"}
+th = {"font":("Helvetica", 15, "underline")}
+tb = {"font":("Helvetica", 12)}
 
 # --------------------------------- Define Layout ---------------------------------
-
-
-left_col = [[sg.Button('Open Bets'), sg.Button('Pending Bets'), sg.Button('My Bets')],
+left_col = [[sg.Button('Callable Bets'), sg.Button('My Bets')],
 			[sg.Listbox(values=[], enable_events=True, size=(40,20), key='-BET_LIST-')]
 			]
 
-right_col = [	[sg.Text('Bet Event', key="-BET_EVENT-")],
-				[sg.Text('Bet Value', key="-BET_VALUE-"), sg.Text('Bet Expiration', key="-BET_EXPIRATION-")],
-			  	[sg.Button("Accept")],
-			  	[sg.Text("_" * 30)],
-				[sg.Text('Make a bet')],
+right_col = [	[sg.Text('Bet Event', **td, **th )],
+				[sg.Text('n/a', key="-BET_EVENT-", **td, **tb)],
+
+				[sg.Text('Bet Value', **td,**th)],
+				[sg.Text('n/a', key="-BET_VALUE-", **td, **tb)],
+
+				[sg.Text('Bet Expiration', **td,**th)],
+				[sg.Text('n/a', key="-BET_EXPIRATION-", **td, **tb)],
+
+			  	[sg.Button("Accept Bet", key="-ACCEPT_BTN-")],
+			  	[sg.Text("_" * 50)],
+
+				[sg.Text('Make a bet', **td, **th)],
+				[sg.Text("Bet Event:", justification="l"), sg.Input(key="-EVENT_INPUT")],
+				[sg.Text("Bet Amount:", justification="l"), sg.Input(key="-AMOUNT_INPUT")],
+				[sg.Text("Bet Expiration (mins):",justification="l"), sg.Input(key="-EXPIRATION_INPUT")],
+				[sg.Button("Send Bet", key="-SEND_BTN-")]
 			]
 
 # ----- Full layout -----
@@ -37,12 +49,12 @@ while True:
 		break
 
 	
-	if event == "Open Bets":
+	if event == "Callable Bets":
 		bet_dict = get_open_bets()
 		open_best_list = [x["event"] for x in bet_dict]
 		window['-BET_LIST-'].update(open_best_list)
 
-	elif event == "Pending Bets":
+	elif event == "My Bets":
 		bet_dict = get_pending_bets()
 		pending_best_list = [x["event"] for x in bet_dict]
 		window['-BET_LIST-'].update(pending_best_list)
@@ -51,6 +63,7 @@ while True:
 		bet = [x for x in bet_dict if x["event"] == values["-BET_LIST-"][0]][0]
 		
 		window["-BET_EVENT-"].update(bet["event"])
+		print(window["-BET_EVENT-"].get_size())
 		window["-BET_VALUE-"].update(bet["amount"])
 		window["-BET_EXPIRATION-"].update(bet["expiration"])
 
