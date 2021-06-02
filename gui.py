23fe63a2b6b6e485387b29d9a0943394face3035
Angus Emmett <gusemmett@gmail.com>
2021-06-02 16:21:38 -0400

    added accept bet functionality

--- gui.py
+++ gui.py
@@ -23,9 +23,9 @@ right_col = [	[sg.Text('Bet Event', **td, **th )],
 			  	[sg.Text("_" * 50)],
 
 				[sg.Text('Make a bet', **td, **th)],
-				[sg.Text("Bet Event:", justification="l"), sg.Input(key="-EVENT_INPUT")],
-				[sg.Text("Bet Amount:", justification="l"), sg.Input(key="-AMOUNT_INPUT")],
-				[sg.Text("Bet Expiration (mins):",justification="l"), sg.Input(key="-EXPIRATION_INPUT")],
+				[sg.Text("Bet Event:", justification="l"), sg.Input(key="-EVENT_INPUT-")],
+				[sg.Text("Bet Amount:", justification="l"), sg.Input(key="-AMOUNT_INPUT-")],
+				[sg.Text("Bet Expiration (mins):",justification="l"), sg.Input(key="-EXPIRATION_INPUT-")],
 				[sg.Button("Send Bet", key="-SEND_BTN-")]
 			]
 
@@ -41,6 +41,9 @@ window = sg.Window('Multiple Format Image Viewer', layout,resizable=True)
 # ----- Run the Event Loop -----
 # --------------------------------- Event Loop ---------------------------------
 bet_dict = {}
+selected_bet = None
+is_on_callable = False
+
 while True:
 	event, values = window.read()
 	if event in (sg.WIN_CLOSED, 'Exit'):
@@ -50,22 +53,41 @@ while True:
 
 	
 	if event == "Callable Bets":
+		window["-ACCEPT_BTN-"].update(disabled = False)
+		is_on_callable = True
+
 		bet_dict = get_open_bets()
 		open_best_list = [x["event"] for x in bet_dict]
 		window['-BET_LIST-'].update(open_best_list)
 
 	elif event == "My Bets":
-		bet_dict = get_pending_bets()
+		window["-ACCEPT_BTN-"].update(disabled = True)
+		is_on_callable = False
+		bet_dict = get_user_bets()
 		pending_best_list = [x["event"] for x in bet_dict]
 		window['-BET_LIST-'].update(pending_best_list)
 
 	elif event == '-BET_LIST-':    # A bet was chosen from the bet box
-		bet = [x for x in bet_dict if x["event"] == values["-BET_LIST-"][0]][0]
-		
-		window["-BET_EVENT-"].update(bet["event"])
-		print(window["-BET_EVENT-"].get_size())
-		window["-BET_VALUE-"].update(bet["amount"])
-		window["-BET_EXPIRATION-"].update(bet["expiration"])
+		selected_bet = [x for x in bet_dict if x["event"] == values["-BET_LIST-"][0]][0]
+
+		window["-BET_EVENT-"].update(selected_bet["event"])
+		window["-BET_VALUE-"].update(selected_bet["amount"])
+		window["-BET_EXPIRATION-"].update(selected_bet["expiration"])
+
+	elif event == "-SEND_BTN-":
+		window["-EVENT_INPUT-"].update("")
+		window["-AMOUNT_INPUT-"].update("")
+		window["-EXPIRATION_INPUT-"].update("")
+
+	elif event == "-ACCEPT_BTN-":
+		if selected_bet != None and is_on_callable:
+			accept_bet(selected_bet["uuid"])
+			bet_dict = get_open_bets()
+			open_best_list = [x["event"] for x in bet_dict]
+			window['-BET_LIST-'].update(open_best_list)
+
+
+
 
 	print(event, values)
 # --------------------------------- Close & Exit ---------------------------------
