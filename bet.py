0b730177e90251ada9748f4fd4091f23fa502920
Fei <i.james.guo@gmail.com>
2021-06-03 14:56:33 +0800

    fix typos and syntax errors

--- bet.py
+++ bet.py
@@ -13,13 +13,13 @@ class Bet:
     """
     Bet is the parent class object with header identifying it as such
     """
+
     def __init__(self):
         """
         Structure of a Bet
         @param header: a header so it can be read that this is a bet
         """
         self.header = 'bet'  # Used to identify the broadcast message is a Bet < do I need to change this?
-      
 
 
 class OpenBet(Bet):
@@ -34,24 +34,23 @@ class OpenBet(Bet):
         """
         super().__init__()
         if id == 0:
-            self.id = uuid.uuid1() #random ID for each bet generated with UUID
+            self.id = uuid.uuid1()  # random ID for each bet generated with UUID
         else:
             self.id = id
         self.originator = origin
         self.event_info = info
         self.win_cond = winCond
         self.amt = amt
-        self.expire = time.time() + expiration # current expiration of bets is after one minute
+        self.expire = time.time() + expiration  # current expiration of bets is after one minute
 
     def __repr__(self):
-        return repr('open ' + self.id + " " + self.originator + " " + self.event_info + " " 
-        + self.win_cond + " " + self.amt)
-      
+        return repr('open ' + self.id + " " + self.originator + " " + self.event_info + " "
+                    + self.win_cond + " " + self.amt + self.expire)
 
 
 class ClosedBet(Bet):
 
-     def __init__(self, betId, caller):
+    def __init__(self, betId, caller):
         """
         Structure of a ClosedBet
         @param betId: the id identifieder for the original bet this bet is referring to
@@ -60,13 +59,12 @@ class ClosedBet(Bet):
         super().__init__()
         self.betId = betId
         self.caller = caller
-    
-     def __repr__(self):
+
+    def __repr__(self):
         return repr('closed ' + self.betId + " " + self.caller)
 
-      
 
-class BetList:  
+class BetList:
     """
     BetList keeps the internal list of open bets that are avalible to be called
     BetList is updated after each sucessful 
@@ -74,9 +72,8 @@ class BetList:
 
     def __init__(self, peer: Peer):
         self.peer = peer
-        self.betList = {} # Type dictionary with key:id, value, Bet
+        self.betList = {}  # Type dictionary with key:id, value, Bet
         self.currentRoundBets = []
-
 
     def recieve_bets(self, data):
         """
@@ -84,11 +81,9 @@ class BetList:
         """
         data = data[struct.calcsize("I"):]  # skip message type field
 
-        byte_bet = struct.unpack_from(bet_fmt, data)
-        string_bet =  byte_bet.decode("utf-8") 
+        byte_bet, = struct.unpack_from(bet_fmt, data)
+        string_bet = byte_bet.decode("utf-8")
         self.currentRoundBets.append(self.string_to_bet(string_bet))
-     
-
 
     def collect_bets(self, n):
         """
@@ -96,7 +91,7 @@ class BetList:
         """
         return_list = []
 
-        if len(self.currentRoundBets < n):
+        if len(self.currentRoundBets) < n:
             return_list = self.betList_ts(self.currentRoundBets)
             self.update_bets(self.currentRoundBets)
             self.currentRoundBets = []
@@ -105,9 +100,7 @@ class BetList:
             self.update_bets(self.currentRoundBets[0:n])
             self.currentRoundBets = self.currentRoundBets[n:]
 
-        
         return return_list
-        
 
     def update_bets(self, updatedBets):
         """
@@ -115,29 +108,27 @@ class BetList:
         @param updatedBets: list of string bets that are confirmed from chain
         """
         for bet in updatedBets:
-            if isinstance(bet, ClosedBet): 
-                self.betList.pop(bet.betId) #remove all bets that were closed
+            if isinstance(bet, ClosedBet):
+                self.betList.pop(bet.betId)  # remove all bets that were closed
             elif isinstance(bet, OpenBet):
-                 self.betlist[bet.id] = bet # add sucessully placed bets
+                self.betList[bet.id] = bet  # add sucessully placed bets
 
-        #remove expired bets
+        # remove expired bets
         for bet in self.betList.items():
-                 if self.is_expired(bet[1]):
-                     self.betList.pop(bet[0])
+            if self.is_expired(bet[1]):
+                self.betList.pop(bet[0])
 
-
-    def place_bet(self, origin, info, winCond, amt):
+    def place_bet(self, origin, info, winCond, amt, expiration):
         """
         Returns new open bet object (as a string) to peer to be braodcasted
         """
-        newBet = OpenBet(0, origin, info, winCond, amt)
+        newBet = OpenBet(0, origin, info, winCond, amt, expiration)
         request = struct.pack("I", MessageType.NEW_BET)
         request += struct.pack(bet_fmt, bytes(repr(newBet)), 'utf-8')
         self.peer.send_signed_data(request)
 
         self.currentRoundBets.append(newBet)
         return repr(newBet)
-        
 
     def call_bet(self, betId, caller):
         """
@@ -147,7 +138,7 @@ class BetList:
 
         newClosedBet = ClosedBet(betId, caller)
         if betId not in self.betList or self.is_expired(self.betList[betId]):
-            return    #check the it isn't expired
+            return  # check the it isn't expired
         else:
             request += struct.pack(bet_fmt, bytes(repr(newClosedBet)), 'utf-8')
 
@@ -155,7 +146,6 @@ class BetList:
 
         self.currentRoundBets.append(newClosedBet)
         return repr(newClosedBet)
-
 
     def get_open_bets(self):
         """
@@ -168,7 +158,6 @@ class BetList:
 
         return open_bets
 
-
     def get_user_bets(self, userId):
         """
         Returns list of bets for a given user
@@ -176,30 +165,27 @@ class BetList:
         """
         userBets = []
         for bet in self.betList.values():
-                 if not self.is_expired(bet) and bet.originator == userId:
-                        userBets.append(repr(bet))
+            if not self.is_expired(bet) and bet.originator == userId:
+                userBets.append(repr(bet))
         return userBets
-
 
     def is_expired(self, openbet):
         return time.time() > openbet.expire
 
-    
     def string_to_bet(self, stringBet):
         """
         helper function that translates from strings to bets 
         """
-        strBet = stringBet.split( )
+        strBet = stringBet.split()
         if strBet[0].equals('open'):
-            return OpenBet(strBet[1], strBet[2], strBet[3], strBet[4], strBet[5])
-        if strBet[0].equals('closed'):     
-            return ClosedBet(strBet[1], strBet[2]) 
+            assert len(strBet[1:]) == 6  # Six params
+            return OpenBet(*strBet[1:])
+        if strBet[0].equals('closed'):
+            return ClosedBet(strBet[1], strBet[2])
 
     def betList_ts(self, betlist):
         stringBets = []
         for bet in betlist:
-            stringBets.append(bytes(repr(bet)), 'utf-8')
+            stringBets.append(bytes(repr(bet), 'utf-8'))
 
         return stringBets
-
-
